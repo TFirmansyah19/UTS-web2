5b6b0744faa80b6de37508f77773e995e067fa48
Taufik Firmansyah | 17111338 <58102364+TFirmansyah19@users.noreply.github.com>
2019-11-26 11:35:21 +0700

    Add files via upload

---
+++ admin/dasbor/list.php
@@ -0,0 +1,12 @@
+<table id="example1" class="table table-bordered table-striped">
+  <table id="example1" class="table table-bordered table-striped">
+  <thead>
+  <tr>
+  <th>Rendering engine</th>
+  <th>Browser</th>
+  <th>Platform(s)</th>
+  <th>Engine version</th>
+  <th>CSS grade</th>
+  </tr>
+</tbody>
+</table>
---
+++ admin/layout/nav.php
@@ -0,0 +1,71 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4"> 
+  <a> 
+    <span class="logo-lg">Toko Fashion</span> 
+  </a> 
+  <li class="nav-item"> <a href="<?php echo base_url('admin/dasbor')?>" 
+    class="nav-link"> <i class="nav-icon far fa-dashboard text-info"></i>
+    <p>Dasboard</p> </a> 
+  </li>
+
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Pengguna
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../../index.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Data Pengguna</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../../index2.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Tambah Pengguna</p>
+                </a>
+              </li>
+            </ul>
+          
+        </ul>
+      </nav>
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>DataTables</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">DataTables</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">DataTable with minimal features & hover style</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
